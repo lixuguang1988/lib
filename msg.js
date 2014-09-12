16de6f95e73dc7778fc9224913849fe008f4e04c
lixuguang88 <lixuguang_1988@163.com>
2014-09-12 17:25:29 +0800

    Update msg.js

--- msg.js
+++ msg.js
@@ -1,40 +1,43 @@
-function msg(options){
-    var defaults = {
-        width : '200px',
-        height : 'auto',
-        offset : '20px',
-        timer : null
-    };
-    var config = $.extend(defaults, options);
+;(function($){
+	$.msg =  function(options){
+	    var defaults = $.extend({
+	        width : '200px'
+	    }, options);
+	    
+	    //关闭已有窗口
+	    $("#msg").remove();
 
-    $("#msg").remove();
-    if(config.timer){clearTimeout(config.timer);}
-    $('body').append('<div id="msg" class="msg '+ config.wrapClass +'" style="width:'+ config.width +';height:'+ config.height +';margin-left:'+ -Math.ceil(parseInt(config.width)/2) +'px;">' + config.message + '</div>');
-    if(config.delay){
-        config.timer = setTimeout('closeMsg()', config.delay);
-    }
-}
+	    $('body').append('<div id="msg" class="msg '+ defaults.wrapClass +'" style="width:'+ defaults.width +';margin-left:'+ -Math.ceil(parseInt(defaults.width)/2) +'px">' + defaults.message + '</div>');
+	    
+	    //自动关闭窗口
+	    if(defaults.delay){
+	        defaults.timer = setTimeout('$.closeMsg()', defaults.delay);
+	    }	
+	};
+	
+	$.closeMsg =  function(){
+	    $("#msg").animate({
+	    	opacity : 0
+	    	}, 800, function(){
+	        $("#msg").remove();
+	    });
+	};
 
-function closeMsg(){
-    $("#msg").animate({
-    	opacity : 0
-    	}, 800, function(){
-        $("#msg").remove();
-    });
-}
-
+}(jQuery));
 
 /*
-.msg{background: #000;padding:10px;color:#fff;font-size: 14px;position: fixed;width: 200px;top: 45%;left:50%;text-align: center;
+.msg{background: #000;padding:10px;color:#fff;font-size: 12px;position: absolute;width: 200px;top: 25%;left:50%;text-align: center;
     -webkit-border-radius: 10px;
         -moz-border-radius:10px;
             border-radius: 10px;
 }
+.danger-msg{background:#c00;text-shadow:0 -1px 0 #900}
 
 msg({
   message : "提示消息",
+  width: "200px",
   delay : 1000, //1s之后关闭
-  wrapClass : 'wrap-msg'
+  wrapClass : 'danger-msg'  // [danger-msg]
 });
 
 */
