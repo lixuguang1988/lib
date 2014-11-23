47891b6c03b3f52a7911ddf9c5cdef8773f4cc3b
lixuguang88 <lixuguang_1988@163.com>
2014-11-23 14:53:01 +0800

    cookie
    baidu

---
+++ example/cookie.js
@@ -0,0 +1,17 @@
+var Cookie = {
+  set: function(c, e, d, f, a, b) {
+    document.cookie = c + "=" + (b ? e : escape(e)) + ((a) ? "; expires=" + a.toGMTString() : "") + ((f) ? "; path=" + f : "; path=/") + ((d) ? "; domain=" + d : "")
+  },
+  get: function(c, b) {
+    var a = document.cookie.match(new RegExp("(^| )" + c + "=([^;]*)(;|$)"));
+    if (a != null) {
+      return unescape(a[2])
+    }
+    return b
+  },
+  clear: function(a, c, b) {
+    if (this.get(a)) {
+      document.cookie = a + "=" + ((c) ? "; path=" + c : "; path=/") + ((b) ? "; domain=" + b : "") + ";expires=Fri, 02-Jan-1970 00:00:00 GMT"
+    }
+  }
+};
