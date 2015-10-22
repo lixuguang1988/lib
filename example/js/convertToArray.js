4ea990fc67011282ea2cefdf1a93fca2d13cb8f8
lixuguang88 <lixuguang_1988@163.com>
2015-10-22 09:41:21 +0800

    Create convertToArray.js

---
+++ example/js/convertToArray.js
@@ -0,0 +1,13 @@
+
+function convertToArray(nodes){
+  var array = null;
+  try{
+    array = Array.prototype.slice(nodes, 0);
+  }catch(ex){
+    array = [];
+    for(var i = 0, len = nodes.length; i < len; i++){
+      array.push(nodes[i]);
+    }
+  }
+  return array;
+}
