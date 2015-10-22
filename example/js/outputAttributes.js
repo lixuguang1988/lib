33e7b38e5ac4a0ebb82ad84539557567fc46d4a4
lixuguang88 <lixuguang_1988@163.com>
2015-10-22 16:42:48 +0800

    Update outputAttributes.js

--- example/js/outputAttributes.js
+++ example/js/outputAttributes.js
@@ -6,12 +6,16 @@ function outputAttributes(element){
         attrName,
         attrValue;
     
-    for( ; i < len ; i++){
+    while( i < len ){
+        //只返回指定值的属性
+        //ie7<=会把没设置属性的属性都打印出来
+        //class = "" 是设置的属性
         if(attrs[i].specified){
             attrName = attrs[i].nodeName;
             attrValue = attrs[i].nodeValue;
             pairs.push(attrName + "=" + attrValue;
         }
+        i++;
     }
     pairs.join(";");
 }
