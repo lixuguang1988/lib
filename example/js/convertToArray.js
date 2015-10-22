1db7932effedef97dd08cf49fa5b6eb389450d79
lixuguang88 <lixuguang_1988@163.com>
2015-10-22 09:43:36 +0800

    Update convertToArray.js

--- example/js/convertToArray.js
+++ example/js/convertToArray.js
@@ -1,8 +1,8 @@
-
+//ie8以前的node是com对象
 function convertToArray(nodes){
     var array = null;
     try{
-        array = Array.prototype.slice(nodes, 0);
+        array = Array.prototype.slice.call(nodes, 0);
     }catch(ex){
         array = [];
         for(var i = 0, len = nodes.length; i < len; i++){
