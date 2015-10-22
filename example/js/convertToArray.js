6b47ebaad24cf7a5e2ecd8c186734e891cec36b1
lixuguang88 <lixuguang_1988@163.com>
2015-10-22 09:42:28 +0800

    Update convertToArray.js

--- example/js/convertToArray.js
+++ example/js/convertToArray.js
@@ -1,13 +1,13 @@
 
 function convertToArray(nodes){
-  var array = null;
-  try{
-    array = Array.prototype.slice(nodes, 0);
-  }catch(ex){
-    array = [];
-    for(var i = 0, len = nodes.length; i < len; i++){
-      array.push(nodes[i]);
+    var array = null;
+    try{
+        array = Array.prototype.slice(nodes, 0);
+    }catch(ex){
+        array = [];
+        for(var i = 0, len = nodes.length; i < len; i++){
+          array.push(nodes[i]);
+        }
     }
-  }
-  return array;
+    return array;
 }
