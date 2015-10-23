c7e5520c9201644c569b6b4d349416efc895935f
lixuguang88 <lixuguang_1988@163.com>
2015-10-23 14:07:28 +0800

    Update loadScript.js

--- example/js/loadScript.js
+++ example/js/loadScript.js
@@ -6,7 +6,7 @@ function loadScript(url){
 }
 
 
-//IE把script视为一个特殊的节点，不允许DOM访问的它的子节点.但是可以通过text属性指定javascript代码
+//IE<=8把script视为一个特殊的节点，不允许DOM访问的它的子节点.但是可以通过text属性指定javascript代码
 //safari 3.0之前不能正确支持text属性
 function loadScriptString(code){
     var script = document.createElement("script");
