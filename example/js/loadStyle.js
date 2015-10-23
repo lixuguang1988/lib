37ae4d9981fbfc22233895d6476ad6a76d8f82a4
lixuguang88 <lixuguang_1988@163.com>
2015-10-23 14:12:46 +0800

    Update loadStyle.js

--- example/js/loadStyle.js
+++ example/js/loadStyle.js
@@ -1,1 +1,21 @@
-function
+function loadStyle(url){
+    var link = document.createElement("link");
+    link.type= "text/style";
+    link.rel="stylesheet";
+    link.href = url;
+    document.getElementsByTagName("head")[0].appendChild(link);
+}
+
+
+//IE<=8把style视为一个特殊的节点，不允许DOM访问的它的子节点.但是可以通过styleSheet属性的cssText来设置
+//safari 3.0之前不能正确支持text属性
+function loadStyleString(code){
+    var style = document.createElement("style");
+    style.type= "text/css";
+    try{
+        style.appendChild(document.createTextNode(code));
+    }catch(ex){
+        style.styleSheet.cssText = code;
+    }
+    document.getElementsByTagName("head")[0].appendChild(style);
+}
