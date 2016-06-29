212a1884e9e61d9eec54eb855343eca3e0142e53
lixuguang88 <lixuguang_1988@163.com>
2016-06-29 16:48:25 +0800

    Update msg.js

--- msg.js
+++ msg.js
@@ -19,53 +19,6 @@
 	    	"left" : "50%",
 	    	"margin-left" : (- Math.ceil(defaults.width/2) ) + "px",
 	    	"background" : "#000",
-	    	"opacity" : ".8"
-	    });
-	    
-	    $close.on("click", function(){
-	    	$message.animate({
-	    		opacity : 0,
-	    	}, defaults.ease, function(){
-	    		$message.remove();
-	    	})	
-	    });
-	    
-	    //自动关闭窗口
-	    if(defaults.delay){
-	        defaults.timer = setTimeout(function(){
-	        	$close.click();
-	        }, defaults.delay);
-	    }	
-	};
-	
-	$.message = Message;
-
-}(jQuery));
-
-undefined
-$.message({message : "wowow"});
-undefined
-;(function($){
-	function Message(options){
-	    var defaults = $.extend({
-	        width : 200,
-	        ease : 500,
-	        wrapClass : "warn"
-	    }, options);
-	    
-	    var $message = $('<div  class="message message-'+ defaults.wrapClass +'"><div class="message-main">' + defaults.message + '</div></div>'),
-	    	$close = $('<a href="javascript:void(0);">&#x000D7;</a>');
-	    	
-	    $message.append($close);
-	    $('body').append($message);
-	    
-	    $message.css({
-	    	"position" : "fixed",
-	    	"top" : "0",
-	    	"width" : defaults.width + "px",
-	    	"left" : "50%",
-	    	"margin-left" : (- Math.ceil(defaults.width/2) ) + "px",
-	    	"background" : "#000",
 	    	"opacity" : ".8",
 	    	"color" : "#fff"
 	    });
